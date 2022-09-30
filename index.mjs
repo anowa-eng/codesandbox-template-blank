6214d7209dd3153fa3c3e9f002b9903b220630a0
anowa-eng <georgemorrissette2010@gmail.com>
2022-09-30 03:40:22 +0000

    nb,nmb,nzcjlkjvljvh

--- index.mjs
+++ index.mjs
@@ -1,12 +1,12 @@
-import express from 'express';
-import prisma from '@prisma/client'
+import express from "express";
+import prisma from "@prisma/client";
 
 const app = express();
 
-app.get('/room/create', function (req, res) {
-    prisma.
+app.get("/room/create", function (req, res) {
+  console.log(prisma.room);
 });
 
 app.listen(8080);
 
-console.log('listening')
+console.log("listening");
